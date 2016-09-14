66fdf3cba95c350bb01d1cd2ed258fc712c7b8f9
vanstee <patrick@vanstee.me>
2016-09-14 11:12:38 -0400

    Don't strip policy_ids from users while iterating

--- lib/cog_cmd/pagerduty/oncall.rb
+++ lib/cog_cmd/pagerduty/oncall.rb
@@ -27,30 +27,48 @@ class CogCmd::Pagerduty::Oncall < Cog::Command
   private
 
   def oncall
-    users = @client.get('users/on_call')['users'].map { |user, acc|
-      {
-        name: user.name,
-        id: user.id,
-        email: user.email,
-        policy_ids: user.on_call.map { |oc| oc.escalation_policy.id }
-      }
-    }
+    users = get_oncall_users
     services = get_services
 
-    services.map { |service|
+    services.map do |service|
+      oncalls = find_oncalls_for_service(service, users)
+
       {
         name: service.name,
         id: service.id,
-        oncall: users.find { |user|
-          user[:policy_ids].include?(service.escalation_policy.id)
-        }.delete_if { |key, value| key == :policy_ids }
+        oncall: oncalls
       }
-    }
+    end
   end
 
   def get_services
     query = request.args.join(" ")
-    @client.get('services', include: ['escalation_policy'], query: query)['services']
+    response = @client.get('services', include: ['escalation_policy'], query: query)
+    response['services']
   end
 
+  def get_oncall_users
+    response = @client.get('users/on_call')
+    users = response['users']
+
+    users.map do |user|
+      policy_ids = Array(user.on_call).map { |oc| oc.escalation_policy.id }
+
+      {
+        name: user.name,
+        id: user.id,
+        email: user.email,
+        policy_ids: policy_ids
+      }
+    end
+  end
+
+  def find_oncalls_for_service(service, users)
+    policy_id = service.escalation_policy.id
+    oncalls = users.select { |u| u[:policy_ids].include?(policy_id) }
+
+    oncalls.map do |user|
+      Hash[user.reject { |key, _| key == :policy_ids }]
+    end
+  end
 end
